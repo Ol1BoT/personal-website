dbda79ba1b9a28aeae2bd060ca8028d28a6736f1
Ollie Mignot <olliemignot@gmail.com>
2021-01-10 17:40:57 +1100

    Started doing own HTML and CSS without Go for development speed

--- main.go
+++ main.go
@@ -10,13 +10,16 @@ import (
 )
 
 var homeView *views.View
+var newHomeView *views.View
 
 func main() {
 	homeView = views.NewView("views/home.gohtml")
+	newHomeView = views.NewView("views/new.gohtml")
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", home)
+	r.HandleFunc("/new", newHome)
 	fmt.Println("Listening on port 3000")
 	http.ListenAndServe(":3000", r)
 }
@@ -25,6 +28,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 
 	if err := homeView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func newHome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	if err := newHomeView.Render(w, nil); err != nil {
 		panic(err)
 	}
 }
